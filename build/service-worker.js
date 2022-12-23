7442ffc1f0687343a2ca87159b25f697e7a0a8a8
Srishti Shukla <srishtishukla66721@gmail.com>
2022-12-23 19:51:03 +0530

    Added firebase config

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a49884a7a4d8a1b8f1857b101cc639f7.js"
+  "/precache-manifest.43fd8b5e997aefe8141744fb547a430a.js"
 );
 
 self.addEventListener('message', (event) => {
